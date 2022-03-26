8ee16773d9fe099d5258b42101e08aa676f64d81
Grace Wang <ewang329@gatech.edu>
2022-03-25 21:52:36 -0400

    Add attraction content page

--- components/trips/detail/leftPane/BoxContainer.js
+++ components/trips/detail/leftPane/BoxContainer.js
@@ -1,15 +1,14 @@
 import styles from '../../../../styles/trips/detail/leftPane/BoxContainer.module.css'
 
 export default function BoxContainer({ title, details }) {
-
     return (
-        <div className={styles.boxContainer}>
+        <a href={'/attractions/' + title.replace(/ /g, '-').toLowerCase()} className={styles.boxContainer}>
             <span className={styles.miniCircle}></span>
             <span className={styles.dottedLine}></span>
             <div className={styles.box}>
                 <h3>{title}</h3>
                 {details.map(d => (<p key={d}>{d}</p>))}
             </div>
-        </div>
+        </a>
     )
 }
---
+++ pages/attractions/[name].js
@@ -0,0 +1,44 @@
+import { useRouter } from 'next/router'
+import styles from '../../styles/attractions/Index.module.css'
+import Webpage from '../../components/trips/detail/rightPane/Webpage'
+
+
+export default function AttractionIndex() {
+    const router = useRouter()
+    const { name } = router.query
+
+    const data = {
+        title: 'Some attraction',
+        images: ['https://picsum.photos/200', 'https://picsum.photos/200'],
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        webpages: [
+            {
+                title: "Webpage name",
+                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            },
+            {
+                title: "Webpage name",
+                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            },
+            {
+                title: "Webpage name",
+                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            }
+        ]
+    }
+ 
+    return (
+        <div className={styles.container}>
+            <h3>{data.title}</h3>
+            <div className={styles.imageContentContainer}>
+                <div className={styles.imageContainer}>
+                    <img src={data.images[0]} />
+                </div>
+                <div className={styles.webpageContent}>
+                    <Webpage />
+                </div>
+            </div>
+            <p>{data.description}</p>
+        </div>
+    )
+}
