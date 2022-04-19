6afa7916b9c84dbca7a8b7ef957c16610896a0de
Grace Wang <ewang329@gatech.edu>
2022-04-19 17:27:02 -0400

    Update drop zone to place box and add hotel

--- components/trips/detail/leftPane/BoxContainer.js
+++ components/trips/detail/leftPane/BoxContainer.js
@@ -5,9 +5,15 @@ export default function BoxContainer({ title, details }) {
         <a href={'/attractions/' + title.replace(/ /g, '-').toLowerCase()} className={styles.boxContainer}>
             <span className={styles.miniCircle}></span>
             <span className={styles.dottedLine}></span>
-            <div className={styles.box}>
+            <div className={`${styles.box} dropzone`}>
                 <h3>{title}</h3>
-                {details.map(d => (<p key={d}>{d}</p>))}
+                {details.length > 1 ? (
+                    <div className={styles.flightDetails}>
+                        <p>Flight #: {details[0]}</p>
+                        <p>Flight time: {details[1]}</p>
+                    </div>
+                ) : <p>{details[0]}</p>
+                }
             </div>
         </a>
     )
